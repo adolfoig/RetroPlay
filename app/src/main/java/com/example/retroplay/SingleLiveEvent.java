afdbb28ebb508bdbe8f180082a1f8c06ee0c62f0
adolfoig <adolfoig04@gmail.com>
2025-05-28 12:18:16 +0200

    Creado archivo para mostrar toast de puntuaciones

---
+++ app/src/main/java/com/example/retroplay/SingleLiveEvent.java
@@ -0,0 +1,32 @@
+package com.example.retroplay;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class SingleLiveEvent<T> extends MutableLiveData<T> {
+    private final AtomicBoolean pending = new AtomicBoolean(false);
+
+    @Override
+    public void observe(@NonNull LifecycleOwner owner, @NonNull Observer<? super T> observer) {
+        super.observe(owner, t -> {
+            if (pending.compareAndSet(true, false)) {
+                observer.onChanged(t);
+            }
+        });
+    }
+
+    @Override
+    public void setValue(T value) {
+        pending.set(true);
+        super.setValue(value);
+    }
+
+    public void call() {
+        setValue(null);
+    }
+}
+
